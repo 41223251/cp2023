f710156d4f342916d2c4c322af8e89c76e093eff
41223251 <144990130+41223251@users.noreply.github.com>
2024-01-15 20:05:21 +0800

    新增ansic 練習5程式碼

---
+++ p5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    int x = 25; // 直接賦值為 25
+    int y = 88; // 直接賦值為 88
+    int result;
+
+    result = x * y; // 計算 x 和 y 的乘積
+
+    // 輸出結果
+    printf("Product of %d and %d = %d\n", x, y, result);
+
+    return 0; // 表示正確結束
+}
