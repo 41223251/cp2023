865931c2df344f78b0bb552e119dc687d13084f2
41223251 <144990130+41223251@users.noreply.github.com>
2024-01-15 20:11:17 +0800

    新增 ex_c 練習10程式碼

---
+++ p20.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+/* program to show character array use */
+int main()
+{
+ char arr2[10];/* define array of 10 characters */
+ int i;
+ printf("enter 10 characters \n");
+ for (i = 0;i < 10;i++)
+ {
+ scanf("%c", &arr2[i]);
+ }
+ printf("Your 10 characters are \n");
+ for (i = 0;i < 10;i++)
+ {
+ printf("%c ", arr2[i]);
+ }
+ printf("\n");
+}
